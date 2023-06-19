97af4bd1b9c6157a5aaf5fd3e9e952c0fd0763c2
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-19 11:26:15 +0530

    Initial commit

---
+++ src/app/core/createschedule/createschedule.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createschedule',
+  templateUrl: './createschedule.component.html',
+  styleUrls: ['./createschedule.component.scss']
+})
+export class CreatescheduleComponent {
+
+}
