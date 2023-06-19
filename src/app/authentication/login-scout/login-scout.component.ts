b6857a528b9a72c28f4ab29df8a0672d70aa232c
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-19 11:26:11 +0530

    Initial commit

---
+++ src/app/authentication/login-scout/login-scout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-scout',
+  templateUrl: './login-scout.component.html',
+  styleUrls: ['./login-scout.component.scss']
+})
+export class LoginScoutComponent {
+
+}
