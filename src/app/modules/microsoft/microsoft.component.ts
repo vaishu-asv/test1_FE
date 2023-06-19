c4218a05b9f3f3124cc5ffbbbb666f09bb28f648
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-19 11:26:09 +0530

    Initial commit

---
+++ src/app/modules/microsoft/microsoft.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-microsoft',
+  templateUrl: './microsoft.component.html',
+  styleUrls: ['./microsoft.component.scss']
+})
+export class MicrosoftComponent {
+
+}
